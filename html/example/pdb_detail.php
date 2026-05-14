eeb4d6cea1fbee55e1b2623c82bb0a1bda88b78f
fuji3to4 <sfujii@bio.kyutech.ac.jp>
2026-05-14 17:31:59 +0900

    fix: Update image source path in pdb_detail.php for correct loading

--- html/example/pdb_detail.php
+++ html/example/pdb_detail.php
@@ -48,7 +48,7 @@ if ($row === null) {
   </ul>
 
   <img class="img-fluid"
-       src="../examples/pic/<?= htmlspecialchars((string)($row['pdbid'] ?? ''), ENT_QUOTES) ?>.jpeg"
+       src="./pic/<?= htmlspecialchars((string)($row['pdbid'] ?? ''), ENT_QUOTES) ?>.jpeg"
        alt="<?= htmlspecialchars((string)($row['pdbid'] ?? ''), ENT_QUOTES) ?>">
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
